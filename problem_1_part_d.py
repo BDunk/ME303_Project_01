4591b5b4316640456a7895e0f66d7789875a75f5
Ben Dunk <ben@bidmii.com>
2021-06-30 13:12:24 -0400

    finish all parts of it maybe?

--- problem_1_part_d.py
+++ problem_1_part_d.py
@@ -2,8 +2,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 start_time = 0
-end_time = 10 ** -3
-time_step = 10 ** -6
+end_time = 0.000347
+time_step = 10 ** -5
 p_0=1.1*10**5
 v_0 = 0
 rho=10**3
@@ -12,11 +12,16 @@ lambda_squared = 3*p_0/(rho*r_0**2)
 constant_term = -(3/2)*p_0/(rho*r_0)
 
 # analytical stuff (not implemented, just random numbers to make the plot work)
-analytical = {'u': [r_0], 'v': [v_0], 't': [start_time]}
+
+analytical = {'u': [r_0], 't': [start_time]}
+smallest = r_0
 for ii in range(round((end_time-start_time)/time_step)):
-    analytical['u'].append(r_0/3)
-    analytical['v'].append(v_0/3)
-    analytical['t'].append((ii+1)*(time_step))
+
+    t_i = analytical['t'][ii]
+    t_i_plus_1 = t_i + time_step
+    u_i_plus_1 = ((1.5*r_0*np.cos(np.sqrt(lambda_squared)*t_i_plus_1)+v_0/np.sqrt(lambda_squared)*np.sin(np.sqrt(lambda_squared)*t_i_plus_1))/3+0.5*r_0)
+    analytical['u'].append(u_i_plus_1)
+    analytical['t'].append(t_i_plus_1)
 
 plot = plt.plot(np.array(analytical['t']), np.array(analytical['u']), label = 'analytical')
 
@@ -74,9 +79,42 @@ plot = plt.plot(np.array(rk_2['t']), np.array(rk_2['u']), label = 'rk_2')
 rk_4 = {'u': [r_0], 'v': [v_0], 't': [start_time]}
 
 for ii in range(round((end_time-start_time)/time_step)):
-    rk_4['u'].append(r_0/2)
-    rk_4['v'].append(v_0/2)
-    rk_4['t'].append((ii+1)*(time_step))
+    u_i = rk_2['u'][ii]
+    v_i = rk_2['v'][ii]
+    t_i = rk_2['t'][ii]
+    t_i_plus_1_half = t_i+(time_step/2)
+    t_i_plus_1 = t_i + time_step
+
+    w_at_u_i = constant_term-lambda_squared*(u_i-r_0)
+
+    v_at_u_i = v_i + 1/2 * time_step * w_at_u_i
+
+    u_i_plus_1_half_star = u_i + 1/2 * time_step * v_at_u_i
+
+    w_i_plus_1_half_star = constant_term-lambda_squared*(u_i-r_0)
+
+    v_i_plus_1_half_star = v_i+ 1/2 * time_step * w_i_plus_1_half_star
+
+    u_i_plus_1_half_star_star = u_i + 1/2 * time_step * v_i_plus_1_half_star
+
+    w_i_plus_1_half_star_star = constant_term-lambda_squared*(u_i-r_0)
+
+    v_i_plus_1_half_star_star = v_i + 1/2 * time_step * w_i_plus_1_half_star_star
+
+    u_i_plus_1_star = v_i_plus_1_half_star_star + time_step * v_i_plus_1_half_star_star
+
+    w_i_plus_1_star = constant_term-lambda_squared*(u_i_plus_1_star-r_0)
+
+    w_avg = w_at_u_i / 6 + w_i_plus_1_half_star / 3 + w_i_plus_1_half_star_star / 3 + w_i_plus_1_star / 6
+
+    v = v_i + time_step * w_avg
+    u = u_i + time_step * v
+
+
+
+    rk_4['u'].append(u)
+    rk_4['v'].append(v)
+    rk_4['t'].append(t_i_plus_1)
 
 plot = plt.plot(rk_4['t'], rk_4['u'], label = 'rk_4')
 
